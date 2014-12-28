ddc041d5abff15e27e25295bb8b6d6c17b4c6d60
johnpap96 <giannispapcod7@gmail.com>
2014-12-28 19:00:43 +0200

    Changes on the crawler

--- engine/crawler.py
+++ engine/crawler.py
@@ -16,22 +16,24 @@ from pprint import pprint
 from HTMLParser import HTMLParser
 #from core.filters import *
 #from core.utils import *
-
-logging.basicConfig()
-
-
 global SCANNED_ROBOTS
 global SCANNED_HOSTS
 global PROCESSES
+
 
 SCANNED_ROBOTS = {}
 SCANNED_HOSTS = []
 PROCESSES = []
 
+client = MongoClient(MY_HOST, 27017)
+db = client[MY_DB]
+
 logging.basicConfig(filename='error.log', level=logging.DEBUG)
 
 url_regex = re.compile(r'href=[\'"]?([^\'" >]+)', re.VERBOSE | re.MULTILINE)
 split_regex = re.compile(r'\s+', re.VERBOSE | re.MULTILINE)
+
+
 class MyHTMLParser(HTMLParser):
     def reset_list(self):
         self.data_list = []
@@ -42,23 +44,29 @@ class MyHTMLParser(HTMLParser):
 class pymongo_recorder(object):
     def __init__(self, cursor_object):
         self.cursor_object = cursor_object
+        self.posts = self.cursor_object.lists
+        self.urls = self.cursor_object.urls
+    def record_url(self, url):
+        if self.urls.find({"urls": str(url)}).count() == 0:
+            self.urls.insert({"urls": str(url)})
+            return True
+        return False
 
-    def recorder_db(data, urls):
+
+    def recorder_db(self, data, urls):
         # Database Update
-
         post = {"data": data, "urls": urls}
-        posts = self.cursor_object.lists
-        if posts.find({"data": str(data)}).count() == 0:
+        if self.posts.find({"data": str(data)}).count() == 0:
             try:
-                posts.insert(post)
+                self.posts.insert(post)
             except Exception:
                 logging.exception('Recorder-mongo-db')
         else:
             try:
-                post_list = posts.find({"data": str(data)})
-                post_list["data"] = post_list["data"] + posts["data"]
+                post_list = self.posts.find({"data": str(data)})
+                post_list["data"] = post_list["data"] + post["data"]
                 post_id = post_list["_id"]
-                posts.update({"_id": post_id}, {"$set": post_list})
+                self.posts.update({"_id": post_id}, {"$set": post_list})
             except Exception:
                 logging.exception('Recorder-mongo-db-Update')
 
@@ -67,13 +75,18 @@ class pymongo_recorder(object):
 
 class Crawler(object):
     def __init__(self, max_processes):
-        self.queue = Queue()
+        self.manager = multiprocessing.Manager()
+        self.queue = self.manager.Queue()
         self.processes = {}
         for process in range(0, max_processes):
             self.processes[process] = Worker(self.queue)
+            multiprocessing.freeze_support()
             self.processes[process].start()
     def add_website(self, website_url):
         self.queue.put((website_url, 0))
+    def connect_processes(self, db):
+        for process in self.processes:
+            self.processes[process].connect_with_pymongo(db)
 
 
 class Worker(multiprocessing.Process):
@@ -81,47 +94,51 @@ class Worker(multiprocessing.Process):
         super(Worker, self).__init__()
         self.queue = queue
         self.parser = MyHTMLParser()
+        self.queue_item = None
+    def connect_with_pymongo(self, db):
+        self.pymongo = pymongo_recorder(db)
     def run(self):
         while True:
-            item = self.queue.get()
-            self.work(item)
-            self.queue.task_done()
+            try:
+                item = self.queue.get()
+            except:
+                pass
+            else:
+                self.work(item)
+                self.queue.task_done()
     def work(self, queue_item):
-        self.parser.reset_list()
-        self.req = urllib2.Request(queue_item[0])
-        self.req.add_header('User-agent', 'Hurricane/0.1')
+        logging.info(str(queue_item))
         try:
-            self.url = urllib2.urlopen(req)
-            self.data = self.url.read()
-            self.urls = re.findall(r'href=[\'"]?([^\'" >]+)', data)
-            self.parser.feed(self.data)
+            logging.info("ok - 0")
+            if self.pymongo.record_url(queue_item[0]):
+                logging.info("ok - 1")
+                self.parser.reset_list()
+                self.req = urllib2.Request(queue_item[0])
+                self.req.add_header('User-agent', 'Hurricane/0.1')
+                logging.info("ok - 2")
+                self.url = urllib2.urlopen(req)
+                self.data = self.url.read()
+                self.urls = re.findall(url_regex, data)
+                self.parser.feed(self.data)
+                logging.info("ok - 3")
+                logging.info(str(self.urls))
+                self.data = "".join(self.parser.data_list)
+                self.data = self.data.replace('\n', '')
+                self.data = self.data.replace('\t', '')
+                self.data = self.data.replace('\r', '')
+                self.data = re.split(split_regex , self.data)
+                logging.info("ok - 4")
+                logging.info(str(self.data))
+                for url in self.urls:
+                    if queue_item[1] + 1 <= 2:
+                        self.queue.put((url, queue_item[1] + 1))
+                for data in self.data:
+                    recorder_db(data, queue_item[0])
+                logging.info("ok - 5")
+        except Exception:
+            logging.exception('Worker-Exception')
 
-            self.data = "".join(self.parser.data_list)
-            self.data = self.data.replace('\n', '')
-            self.data = self.data.replace('\t', '')
-            self.data = self.data.replace('\r', '')
-            self.data = re.split(split_regex , self.data)
-            for url in self.urls:
-                self.queue.put((url, queue_item + 1))
-        except:
-            pass
-
-"""
-req = urllib2.Request('')
-req.add_header('User-agent', 'Hurricane/0.1')
-r = urllib2.urlopen(req)
-data = r.read()
-#pprint(re.findall(r'href=[\'"]?([^\'" >]+)', data))
-parser = MyHTMLParser()
-parser.reset_list()
-parser.feed(data)
-#pprint(parser.data_list)
-data = "".join(parser.data_list)
-
-data = data.replace('\n', '')
-data = data.replace('\t', '')
-data = data.replace('\r', '')
-
-pprint(re.split(r"\s+", data))
-#pprint(re.findall("(https?:\/\/)?([\da-z\.-]+)\.([a-zA-Z\.]{2,6})([\/\w \.-]*)*\/?", data))
-"""
+if __name__ == '__main__':
+    crawl = Crawler(2)
+    crawl.connect_processes(db)
+    crawl.add_website("http://koslib.com/")
